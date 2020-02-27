83ee3f1cc914fae6ac0e6eaba56f9a7dc8b1467a
HarshalShende <harshalshende357@gmail.com>
2020-02-27 16:13:10 +0530

    show camp page build fully responsive

--- admin_add_camps.php
+++ admin_add_camps.php
@@ -21,6 +21,88 @@ if(!isset($_SESSION['usertype']))
 				
 				<div class="col-sm-9" >
 					<h3 class="text-primary"><i class="fa fa-home"></i> Camps </h3><hr>
+
+					<?php
+					if(isset($_POST["submit"]))
+								{
+						$target_dir = "camp_image/";
+						$img="camp_image/noimage.jpg";
+						$target_file = $target_dir.rand(100,999). basename($_FILES["fileToUpload"]["name"]);
+						$uploadOk = 1;
+						$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+						// Check if image file is a actual image or fake image
+						$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+						if($check !== false) {
+						echo "";
+						$uploadOk = 1;
+						} else {
+						//  echo "File is not an image.";
+						$uploadOk = 0;
+						}
+						// Check if file already exists
+						if (file_exists($target_file)) {
+						// echo "Sorry, file already exists.";
+						$uploadOk = 0;
+						}
+						// Check file size
+						if ($_FILES["fileToUpload"]["size"] > 5000000000) {
+						// echo "Sorry, your file is too large.";
+						$uploadOk = 0;
+						}
+						// Allow certain file formats
+						if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+						&& $imageFileType != "gif" ) {
+						// echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+						$uploadOk = 0;
+						}
+						// Check if $uploadOk is set to 0 by an error
+						if ($uploadOk == 0) {
+						// echo "Sorry, your file was not uploaded.";
+						// if everything is ok, try to upload file
+						} else {
+						if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+						$img=$target_file;
+						} else {
+						//   echo "Sorry, there was an error uploading your file.";
+						}
+						}
+						$country="";
+						$state="";
+						$qry="SELECT COUNTRY_NAME FROM country WHERE COUNTRY_ID={$_POST["COUNTRY"]}";
+						$res=$con->query($qry);
+						if($res->num_rows>0)
+						{
+						if($row=$res->fetch_assoc())
+						{
+						$country=$row["COUNTRY_NAME"];
+						}
+						}
+						$qry="SELECT STATE_NAME FROM state WHERE STATE_ID={$_POST["STATE"]}";
+						$res=$con->query($qry);
+						if($res->num_rows>0)
+						{
+						if($row=$res->fetch_assoc())
+						{
+						$state=$row["STATE_NAME"];
+						}
+						}
+						$cityname=$_POST["CITY"];
+						$sql="
+						INSERT INTO camps
+						(TITLE, ORG_BY, COUNTRY, STATE, CITY, AREA, PINCODE, DETAILS, CAMP_PIC, CAMP_DATE)
+						VALUES
+						('{$_POST["TITLE"]}', '{$_POST["ORG_BY"]}', '{$country}', '{$state}', '$cityname', '{$_POST["AREA"]}', '{$_POST["PINCODE"]}', '{$_POST["DETAILS"]}', '{$img}', '{$_POST["CAMP_DATE"]}');";
+								if($con->query($sql))
+									{
+										echo '
+										<div class="alert alert-success">
+																<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+																<strong>Success!</strong> Camp Details Submitted.
+										</div>
+										';
+									}
+							}
+				?>
 					<div class="row centered-form ">
 						<div class="col-xs-12 col-lg-8 col-sm-8 col-md-6 col-lg-offset-2 col-sm-offset-2 col-md-offset-3">
 							<div class="panel panel-primary">
@@ -48,7 +130,7 @@ if(!isset($_SESSION['usertype']))
 													{
 														while($row=$result->fetch_assoc())
 														{
-																					echo "<option value='{$row['COUNTRY_ID']}'>{$row['COUNTRY_NAME']}	</option>";
+																						echo "<option value='{$row['COUNTRY_ID']}'>{$row['COUNTRY_NAME']}	</option>";
 														}
 													}
 												?>
@@ -65,7 +147,7 @@ if(!isset($_SESSION['usertype']))
 													{
 														while($row=$result->fetch_assoc())
 														{
-																					echo "<option value='{$row['STATE_ID']}'>{$row['STATE_NAME']}	</option>";
+																						echo "<option value='{$row['STATE_ID']}'>{$row['STATE_NAME']}	</option>";
 														}
 													}
 													
@@ -84,7 +166,7 @@ if(!isset($_SESSION['usertype']))
 													{
 														while($row=$result->fetch_assoc())
 														{
-																				echo "<option value='{$row['CITY_ID']}'>{$row['CITY_NAME']}	</option>";
+																					echo "<option value='{$row['CITY_ID']}'>{$row['CITY_NAME']}	</option>";
 														}
 													}
 													
@@ -104,6 +186,10 @@ if(!isset($_SESSION['usertype']))
 											<textarea required name="DETAILS" id="DETAILS" rows="5" style="resize:none;"class="form-control" placeholder="Full Description"></textarea>
 										</div>
 										<div class="form-group">
+											<label class="control-label text-primary" for="CAMP_DATE">Camp Date</label>
+											<input type="text"  name="CAMP_DATE" value="0000/00/00" id="CAMP_DATE" placeholder="YYYY/MM/DD" class="form-control input-sm DATES">
+										</div>
+										<div class="form-group">
 											<label class="control-label text-success" for="fileToUpload" >Upload Photo</label>
 											<input type="file" class="form-control"  name="fileToUpload">
 										</div>
@@ -115,164 +201,83 @@ if(!isset($_SESSION['usertype']))
 							</div>
 						</div>
 					</div>
-				</div>
+				</div></div></div>
 				
-					<?php
-						if(isset($_POST["submit"]))
-									{
-							$target_dir = "camp_image/";
-							$img="camp_image/noimage.jpg";
-							$target_file = $target_dir.rand(100,999). basename($_FILES["fileToUpload"]["name"]);
-							$uploadOk = 1;
-							$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-							// Check if image file is a actual image or fake image
-							$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-							if($check !== false) {
-							echo "";
-							$uploadOk = 1;
-							} else {
-							//  echo "File is not an image.";
-							$uploadOk = 0;
-							}
-							// Check if file already exists
-							if (file_exists($target_file)) {
-							// echo "Sorry, file already exists.";
-							$uploadOk = 0;
-							}
-							// Check file size
-							if ($_FILES["fileToUpload"]["size"] > 5000000000) {
-							// echo "Sorry, your file is too large.";
-							$uploadOk = 0;
-							}
-							// Allow certain file formats
-							if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-							&& $imageFileType != "gif" ) {
-							// echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-							$uploadOk = 0;
-							}
-							// Check if $uploadOk is set to 0 by an error
-							if ($uploadOk == 0) {
-							// echo "Sorry, your file was not uploaded.";
-							// if everything is ok, try to upload file
-							} else {
-							if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-							$img=$target_file;
-							} else {
-							//   echo "Sorry, there was an error uploading your file.";
-							}
-							}
-							$country="";
-							$state="";
-							$qry="SELECT COUNTRY_NAME FROM country WHERE COUNTRY_ID={$_POST["COUNTRY"]}";
-							$res=$con->query($qry);
-							if($res->num_rows>0)
-							{
-							if($row=$res->fetch_assoc())
-							{
-							$country=$row["COUNTRY_NAME"];
-							}
-							}
-							$qry="SELECT STATE_NAME FROM state WHERE STATE_ID={$_POST["STATE"]}";
-							$res=$con->query($qry);
-							if($res->num_rows>0)
-							{
-							if($row=$res->fetch_assoc())
-							{
-							$state=$row["STATE_NAME"];
-							}
-							}
-							$cityname=$_POST["CITY"];
-							$sql="
-							INSERT INTO camps
-							(TITLE, ORG_BY, COUNTRY, STATE, CITY, AREA, PINCODE, DETAILS, CAMP_PIC)
-							VALUES
-							('{$_POST["TITLE"]}', '{$_POST["ORG_BY"]}', '{$country}', '{$state}', '$cityname', '{$_POST["AREA"]}', '{$_POST["PINCODE"]}', '{$_POST["DETAILS"]}', '{$img}');";
-									if($con->query($sql))
-										{
-											echo '
-											<div class="alert alert-success">
-																<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-																<strong>Success!</strong> Camp Details Submitted.
-											</div>
-											';
-										}
-								}
-					?>
-					<?php include("admin_footer.php"); ?>
-					<script>
-					$(document).ready(
-					function(){
-							
-							
-							// $("#CITY").change(function(){
+				<?php include("admin_footer.php"); ?>
+				<script>
+				$(document).ready(
+				function(){
+						
+						
+						// $("#CITY").change(function(){
 							// 	var city=$("#CITY").val();
 							// 	//alert(city);
 							// 	$.post('functions.php',{G_CITY_ID:city},function(data){
-							// 						//	alert(data);
-							// 		$("#STATE").html(data);
+													// 						//	alert(data);
+								// 		$("#STATE").html(data);
 							// 	});
-								
-							// });
 							
-							
-							$("#COUNTRY").change(function(){
-								var countr=$("#COUNTRY").val();
-								//alert(city);
-								$.post('get_state.php',{G_STATE_ID:countr},function(data){
+						// });
+						
+						
+						$("#COUNTRY").change(function(){
+							var countr=$("#COUNTRY").val();
+							//alert(city);
+							$.post('get_state.php',{G_STATE_ID:countr},function(data){
 													//	alert(data);
-									$("#STATE").html(data);
-								});
-								
+								$("#STATE").html(data);
 							});
 							
-								$("#STATE").change(function(){
-								var stid=$("#STATE").val();
-								//alert(city);
-								$.post('get_city.php',{G_STATE_ID:stid},function(data){
+						});
+						
+							$("#STATE").change(function(){
+							var stid=$("#STATE").val();
+							//alert(city);
+							$.post('get_city.php',{G_STATE_ID:stid},function(data){
 													//	alert(data);
-									$("#CITY").html(data);
-								});
-								
+								$("#CITY").html(data);
 							});
+							
+						});
+					
 						
 							
-								
-					});
-					
-					
-					$(function() {
-					var availableTags = [
-					<?php
-						$sql="SELECT AREA_NAME FROM area";
-												$result=$con->query($sql);
-												
-												if($result->num_rows>0)
+				});
+				
+				
+				$(function() {
+				var availableTags = [
+				<?php
+					$sql="SELECT AREA_NAME FROM area";
+											$result=$con->query($sql);
+											
+											if($result->num_rows>0)
+											{
+												$i=0;
+												$n=$result->num_rows;
+												while($row=$result->fetch_assoc())
 												{
-													$i=0;
-													$n=$result->num_rows;
-													while($row=$result->fetch_assoc())
+													$i++;
+													if($n!=$i)
 													{
-														$i++;
-														if($n!=$i)
-														{
-															echo '"'.$row['AREA_NAME'].'",';
-														}
-														else
-														{
-															echo '"'.$row['AREA_NAME'].'"';
-														}
+														echo '"'.$row['AREA_NAME'].'",';
 													}
-													
+													else
+													{
+														echo '"'.$row['AREA_NAME'].'"';
+													}
 												}
-						
-						
-					?>
-					];
-					$( "#AREA" ).autocomplete({
-					source: availableTags
-					});
-					});
+												
+											}
 					
-					</script>
-				</body>
-			</html>
+					
+				?>
+				];
+				$( "#AREA" ).autocomplete({
+				source: availableTags
+				});
+				});
+				
+				</script>
+			</body>
+		</html>
--- view_camp.php
+++ view_camp.php
@@ -20,7 +20,7 @@ error_reporting(0);?>
                     <?php  include("blood_bread.php"); ?>
                 </div>
             </div>
-            <div class="container">
+            <div class="container-fluid">
                 <?php
                 
                 $sql="SELECT * FROM camps";
@@ -31,18 +31,22 @@ error_reporting(0);?>
                 while($row=$result->fetch_assoc())
                 {
                 ?>
-                <div class="card hovercard col-lg-3 col-xs-3 col-sm-6 col-md-6 col-12 m-5 ">
-                    <div class="cardheader">
+                <div class="col-lg-4 col-sm-6 col-md-6 col-6 py-2">
+                <div class="card hovercard h-100 " style="margin-top: 5rem; ">
+                    <div class="cardheader border-warning">
                         <div class="avatar text-center">
                             <img alt="" height="80%" width="80%" src="<?php echo $row['CAMP_PIC']; ?>">
                         </div>
                     </div>
                     <div class="card-body info">
-                        <div class="title text-success text-center h1"><?php echo $row['TITLE']; ?></div>
-                        <div class="desc"><?php echo $row['ORG_BY']; ?></div>
-                        <div class="desc text-secondary">Address:<?php echo '{$row["AREA"]}, {$row["CITY"]},<br>{$row["STATE"]},{$row["PINCODE"]}<br>{$row["COUNTRY"]}'; ?></div>
+                        <div class="title text-primary h2 text-center">Title:<?php echo $row['TITLE']; ?></div>
+                        <div class="desc  text-info h3 text-center">Organized By: <?php echo $row['ORG_BY']; ?></div><br>
+                        <div class="desc  text-warning h4 text-center">Address:<?php echo "<td>{$row["AREA"]}, {$row["CITY"]},<br>{$row["STATE"]},{$row["PINCODE"]}<br>{$row["COUNTRY"]}</td>"; ?></div><br>
+                        <div class="desc  text-dark h4 text-center">Camp Date: <?php echo $row['CAMP_DATE']; ?></div><br>
+
                     </div>
                 </div>
+            </div>
                 <?php }
                 }
                 ?>
